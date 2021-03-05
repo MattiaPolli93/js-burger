4cc12d6c35a9d20a2cab9f1290116a36ff9442bf
Mattia <matpolli93@gmail.com>
2021-03-05 15:42:48 +0100

    Added ingredients (value) part

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 // Variables
 var calculate = document.getElementById("calculate");
 var burgerName = document.getElementById("burger-name");
-
+var addIngredients = document.getElementsByClassName("add-ingredients");
 
 // 1. Adding the "click" event to the button (#calculate)
 calculate.addEventListener("click",
  function () {
     // 2. Checking that user inserts burger name
     if ((burgerName.value != "") && (isNaN(burgerName.value))) {
+        // Checking (selected) ingredients
+        var i;
+        var sum = 50; // 50 here matches the initial price of the burger in the footer section (i.e. 50€)
+        
+        for (i = 0; i <= addIngredients.length; i++ ) {
+            // If an ingredient is selected, its value will be added
+            if (addIngredients[i].checked) {
+                sum += parseInt(addIngredients[i].value);
+            }
+        }
     } else {
         alert("Please give a name to your burger! 🍔");
     }
