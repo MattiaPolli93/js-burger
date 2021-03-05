1457d2ad239f48d4a05b0811a24295c48c91932e
Mattia <matpolli93@gmail.com>
2021-03-05 15:29:13 +0100

    Added name validation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 // Variables
-var 
+var calculate = document.getElementById("calculate");
+var burgerName = document.getElementById("burger-name");
+
+
+// 1. Adding the "click" event to the button (#calculate)
+calculate.addEventListener("click",
+ function () {
+    // 2. Checking that user inserts burger name
+    if ((burgerName.value != "") && (isNaN(burgerName.value))) {
+    } else {
+        alert("Please give a name to your burger! 🍔");
+    }
+ }
+);
